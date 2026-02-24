0e18b626846168b59a328cce8a7ac0779b9e83f0
champnz <amir0ghahremanian@gmail.com>
2026-02-24 04:54:37 +0330

    neovim: autopairs instead of autoclose

--- config/nvim/lua/plugins/autoclose.lua
+++
@@ -1,7 +0,0 @@
-local Plugin = { "m4xshen/autoclose.nvim" }
-
-Plugin.config = function()
-	require("autoclose").setup()
-end
-
-return Plugin
---
+++ config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+local Plugin = { 'windwp/nvim-autopairs' }
+
+Plugin.event = "InsertEnter"
+
+Plugin.config = true
+
+-- this is equivalent to setup({}) function
+Plugin.opts = {}
+
+return Plugin

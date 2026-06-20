6d9ba23409bbae4d8825806398d28ea58b3e8b64
champnz <amir0ghahremanian@gmail.com>
2026-06-20 14:11:28 +0330

    toggleterm: escape

--- config/nvim/lua/plugins/toggleterm.lua
+++ config/nvim/lua/plugins/toggleterm.lua
@@ -10,6 +10,14 @@ Plugin.config = function()
 			winblend = 0,
 		}
 	})
+
+	function _G.set_terminal_keymaps()
+		local opts = { buffer = 0 }
+		vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+	end
+
+	-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+	vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 end
 
 return Plugin

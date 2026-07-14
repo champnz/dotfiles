1148b718e774214671b3e71fce64117e6f35a1cd
champnz <amir0ghahremanian@gmail.com>
2026-07-14 15:22:11 +0330

    autopairs: check treesitter

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -2,9 +2,10 @@ local Plugin = { 'windwp/nvim-autopairs' }
 
 Plugin.event = "InsertEnter"
 
-Plugin.config = true
-
--- this is equivalent to setup({}) function
-Plugin.opts = {}
+Plugin.config = function()
+	require("nvim-autopairs").setup({
+		check_ts = true,
+	})
+end
 
 return Plugin
